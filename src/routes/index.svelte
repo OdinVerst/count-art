ec823323dee9bfd4e62e087b0415c6349426dbf5
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2020-11-24 20:47:22 +0300

    Add plug page

---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>Count ART</title>
+</svelte:head>
+
+<div class="main_container">
+    <div class="wrapper">
+    <img src="img/count_logo.png" alt="Логотип">
+    <h1>Сайт находится в разработке</h1>
+    <a href="mailto:admin@countart.ru">
+        <p class="email__link">admin@countart.ru</p></a>
+    </div>
+</div>
+
+

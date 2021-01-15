413debecd548c911a3877b25f08cfb1ddf2a1e4a
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2021-01-15 16:04:35 +0300

    Add routing to detail projects

--- src/components/Projects/projectPreview.svelte
+++ src/components/Projects/projectPreview.svelte
@@ -3,14 +3,23 @@
 </script>
 
 <style>
-    .post-list__img {
+    .post__img {
         width: 100%;
+    }
+
+    .post__title::before {
+        content: '';
+        display: inline-block;
+        width: 100%;
+        height: 2px;
+        background: var(--text-color);
+        margin-bottom: 18px;
     }
 </style>
 
-<article class="post-list__item">
-    <img class="post-list__img" src={post.img} alt="Разработка фирменного стиля Tree Trunk">
-    <h3>{post.name}</h3>
-    <p>дата публикации проекта
-        категориия дизайна</p>
+<article class="post">
+    <img class="post__img" src={post.img} alt="Разработка фирменного стиля Tree Trunk">
+    <h3 class="post__title"><a href={'/projects/' + post.slug}>{post.name}</a></h3>
+    <p>{post.date}</p>
+    <p>{post.category}</p>
 </article>
---
+++ src/routes/projects/[slug].svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export async function preload({ params }) {
+		const resDetailPost = await this.fetch(`projects/${params.slug}.json`);
+		const dataDetail = await resDetailPost.json();
+		
+		if (resDetailPost.status === 200) {
+			return { post: dataDetail };
+		} else {
+			this.error(resDetailPost.status, data.message);
+		}
+	}
+</script>
+
+
+<script>
+    export let post;
+</script>
+
+<svelte:head>
+	<title>{post.title}</title>
+</svelte:head>
+
+<p>{post.name}</p>
